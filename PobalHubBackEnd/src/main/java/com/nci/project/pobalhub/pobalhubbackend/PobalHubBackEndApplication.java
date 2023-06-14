b6e3d759eb5af2baae1f20b02054e6a5b262f5dd
Donal Hanway <x22148957@student.ncirl.ie>
2023-06-14 17:11:13 +0100

    Initial commit

---
+++ PobalHubBackEnd/src/main/java/com/nci/project/pobalhub/pobalhubbackend/PobalHubBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.nci.project.pobalhub.pobalhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PobalHubBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PobalHubBackEndApplication.class, args);
+    }
+
+}
---
+++ PobalHubBackEnd/src/test/java/com/nci/project/pobalhub/pobalhubbackend/PobalHubBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nci.project.pobalhub.pobalhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PobalHubBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
